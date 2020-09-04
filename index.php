bce6ccedce6695cdbe1b565460cec5cc4cd642b7
Ariel Muñoz Villegas <ariel_munoz@contraloria.gob.bo>
2020-09-04 19:11:27 -0400

    Avances_(4-9-20)

--- index.php
+++ index.php
@@ -1,1 +1,139 @@
-<h1>Hola Mundo 1 test</h1>
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Blog de Videojuegos</title>
+</head>
+
+<body>
+    <!--Cabecera-->
+    <header id="cabecera">
+        <!--Logo-->
+        <div id="logo">
+            <a href="index.php">
+                Blog de Videojuegos
+            </a>
+        </div>
+        <!--Menu-->
+        <nav id="menu">
+            <ul>
+                <li>
+                    <a href="index.php">Inicio</a>
+                </li>
+                <li>
+                    <a href="index.php">Categoria 1</a>
+                </li>
+                <li>
+                    <a href="index.php">Categoria 2</a>
+                </li>
+                <li>
+                    <a href="index.php">Categoria 3</a>
+                </li>
+                <li>
+                    <a href="index.php">Categoria 4</a>
+                </li>
+                <li>
+                    <a href="index.php">Sobre Nosotros</a>
+                </li>
+                <li>
+                    <a href="index.php">Contacto</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+
+    <div id="contenedor">
+        <!--Barra Lateral-->
+        <aside id="sidebar">
+            <div id="register" class="bloque">
+                <h3>Identificate</h3>
+                <form action="login.php" method="POST">
+                    <label for="email">Email</label>
+                    <input type="email" name="email">
+                    
+                    <label for="password">Contraseña</label>
+                    <input type="password" name="password">
+
+                    <input type="submit" value="Entrar">
+                </form>
+            </div>
+            <div id="register" class="bloque">
+                <h3>Registrate</h3>
+                <form action="registro.php" method="POST">
+                    <label for="nombre">Nombre</label>
+                    <input type="text" name="nombre">    
+
+                    <label for="apellidos">Apellidos</label>
+                    <input type="text" name="apellidos">
+                    
+                    <label for="email">Email</label>
+                    <input type="email" name="email">
+                    
+                    <label for="password">Contraseña</label>
+                    <input type="password" name="password">
+
+                    <input type="submit" value="Registrar">
+                </form>
+            </div>
+        </aside>
+        
+        <!--Caja Principal-->
+        <div id="principal">
+            <h1>Ultimas entradas</h1>            
+            <article class="entrada">
+                <h2>Titulo de mi entrada</h2>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
+                    labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco 
+                    laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in 
+                    voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                </p>
+            </article>
+            
+            <article class="entrada">
+                <h2>Titulo de mi entrada</h2>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
+                    labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco 
+                    laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in 
+                    voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                </p>
+            </article>
+            
+            <article class="entrada">
+                <h2>Titulo de mi entrada</h2>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
+                    labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco 
+                    laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in 
+                    voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                </p>
+            </article>
+            
+            <article class="entrada">
+                <h2>Titulo de mi entrada</h2>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
+                    labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco 
+                    laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in 
+                    voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                </p>
+            </article>
+            
+            <article class="entrada">
+                <h2>Titulo de mi entrada</h2>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
+                    labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco 
+                    laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in 
+                    voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                </p>
+            </article>
+        </div>
+    </div>
+
+    <!--Pie de Página-->
+</body>
+
+</html>
